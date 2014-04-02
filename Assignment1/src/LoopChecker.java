f014d3e04b157ac71bea7a72767752859fa3ea0c
Mostafa Rizk <mostafa.k.rizk@gmail.com>
2014-04-02 12:11:42 +0000

    Improved complexity description and comments

--- Assignment1/src/LoopChecker.java
+++ Assignment1/src/LoopChecker.java
@@ -13,14 +13,13 @@ public class LoopChecker
     
   
   /** Given a cluster of nodes, determines if they are a loop
-    * If cluster-size is k, and the graph of nodes adjacent to the cluster
-   * has j nodes and l edges then this method has complexity O( 6k^2 + j + l)
+    *
    * @cluster Cluster of colored nodes
    */
   public boolean isLoop(Cluster cluster)
   {
     
-	ArrayList<Position> clusterNodes = cluster.getNodes();
+	  ArrayList<Position> clusterNodes = cluster.getNodes();
     ArrayList<Position> adjacentNodes = new ArrayList<Position>();
     
     //Goes through every node in the cluster, finds adjacent nodes that aren't in the cluster and adds them to a list
@@ -31,7 +30,7 @@ public class LoopChecker
     	//Assumes nodes adjacent to cluster are of a different colour or empty
     	for(Position node : localAdjacentNodes)
     	{
-    		//If the nodes haven't already been added to the list of nodes adjacent to the cluster and they're not part of the cluster
+    		//If the nodes haven't already been added to the list of nodes adjacent to the cluster and they're not part of the clusterr
     	       if( !adjacentNodes.contains(node)
     	        && !clusterNodes.contains(node))
     	       {
@@ -52,8 +51,7 @@ public class LoopChecker
   
   /**
    * Checks if a given list of nodes are one component i.e. all nodes are reachable from all other nodes
-   * This method has complexity O(j+l) where j is the number of nodes and l is the number of 'edges' between
-   * them (bfs complexity)
+   * 
    */
   public boolean isOneComponent(ArrayList<Position> adjacentNodes)
   {
@@ -64,7 +62,7 @@ public class LoopChecker
      
      //Checks all nodes surrounding current node. If any of them are in the list of adjacent nodes, they are added
      // to the queue and removed from the list of adjacent nodes. Once the queue is empty, then all reachable nodes 
-     //have been explored.
+     //will have been explored.
      while(!queue.isEmpty())
      {
        currentNode = queue.remove();
@@ -81,6 +79,7 @@ public class LoopChecker
        
      }
      adjacentNodes.trimToSize();
+     
      //All reachable nodes have been explored. If the list of adjacent nodes is not empty then some nodes
      //were not reachable and there is more than one component
      if(adjacentNodes.size() == 0)
